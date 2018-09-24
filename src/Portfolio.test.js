9a328b00e2f171932a5aa9f5c392981e0b14eb58
Alex Lee <alex@alex-lee.site>
2018-09-24 15:47:58 -0700

    Rename App test to Portfolio test

--- src/Portfolio.test.js
+++ src/Portfolio.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Portfolio from './Portfolio';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Portfolio />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
