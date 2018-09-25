f24a85349462ecb030bcd2e66c5747b2f113afb7
Alex Lee <alex@alex-lee.site>
2018-09-24 21:53:51 -0700

    Change paths and add About Section to Portfolio Component

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types'
 // import { Intro } from './components/Intro'
 import { App } from 'grommet'
 import NavMenu from './components/NavMenu'
-import Intro from './components/Intro'
+import Intro from './sections/Intro'
+import About from './sections/About'
 
 class Portfolio extends Component {
   render() {
@@ -12,6 +13,7 @@ class Portfolio extends Component {
       <App>
         <NavMenu />
         <Intro />
+        <About />
       </App>
     )
   }
