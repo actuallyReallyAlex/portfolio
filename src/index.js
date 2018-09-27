60c8794c1a30fe1bf05f45ba57bd4bdab9f5e21a
Alex Lee <alex@alex-lee.site>
2018-09-27 10:00:56 -0700

    ➕ Dotenv config

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import ReactDOM from 'react-dom'
 import Site from './Site'
 import registerServiceWorker from './registerServiceWorker'
 
+require('dotenv').config()
+
 ReactDOM.render(<Site />, document.getElementById('root'))
 registerServiceWorker()
