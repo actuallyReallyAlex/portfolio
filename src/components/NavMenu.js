a21488f5ffd7080cea225b370c52713aa40ad6f3
Alex Lee <alex@alex-lee.site>
2018-09-28 12:28:52 -0700

    ✨ Add Anchors to NavMenu
    Issue #20.
    
    Classes update on click.
    
    TODO: Issue #41 - Update on scroll.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
 import { Header, Title, Box, Menu, Anchor } from 'grommet'
 
 class NavMenu extends Component {
+  state = { currentSection: 'home' }
+
+  smoothlyScroll = elementId => {
+    document
+      .getElementById(elementId)
+      .scrollIntoView({ behavior: 'smooth', block: 'start' })
+
+    this.setState({ currentSection: elementId })
+  }
   render() {
     return (
-      <Header fixed={true}>
+      <Header pad={{ horizontal: 'medium' }} fixed={true}>
         <Title>Alex Lee</Title>
         <Box flex={true} justify="end" direction="row" responsive={false}>
-          <Menu dropAlign={{ right: 'right' }}>
-            <Anchor href="#" className="active">
-              First
+          <Menu direction="row" size="small" dropAlign={{ right: 'right' }}>
+            <Anchor
+              onClick={() => {
+                this.smoothlyScroll('home')
+              }}
+              className={this.state.currentSection === 'home' ? 'active' : ''}
+            >
+              Home
             </Anchor>
-            <Anchor href="#">Second</Anchor>
-            <Anchor href="#">Third</Anchor>
+            <Anchor
+              onClick={() => this.smoothlyScroll('about')}
+              className={this.state.currentSection === 'about' ? 'active' : ''}
+            >
+              About
+            </Anchor>
+            <Anchor
+              onClick={() => this.smoothlyScroll('resume')}
+              className={this.state.currentSection === 'resume' ? 'active' : ''}
+            >
+              Resume
+            </Anchor>
+            <Anchor
+              onClick={() => this.smoothlyScroll('portfolio')}
+              className={
+                this.state.currentSection === 'portfolio' ? 'active' : ''
+              }
+            >
+              Portfolio
+            </Anchor>
+            <Anchor
+              onClick={() => this.smoothlyScroll('contact')}
+              className={
+                this.state.currentSection === 'contact' ? 'active' : ''
+              }
+            >
+              Contact
+            </Anchor>
           </Menu>
         </Box>
       </Header>
