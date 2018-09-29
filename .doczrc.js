4a467a03bab82af0b7d4a185f4896f1de2e431ec
Alex Lee <alex@alex-lee.site>
2018-09-28 20:41:25 -0700

    🔧 Add Docz Config

---
+++ .doczrc.js
@@ -0,0 +1,6 @@
+export default {
+  ordering: 'ascending'
+  // htmlContext: {
+  //   favicon: 'https://raw.githubusercontent.com/alexlee-dev/bookbag/master/src/assets/favicon/favicon.ico'
+  // }
+}
