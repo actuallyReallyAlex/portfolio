c96d4a8d3d052b8e1805872e22a2f427ba44f1bb
Alex Lee <alex@alex-lee.site>
2018-09-29 09:28:54 -0700

    🏗️ Reinstall Docz and Build Again

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,6 @@
 export default {
   ordering: 'ascending',
   dest: '/src/docs/',
-  base: '/docs/',
   htmlContext: {
     favicon: 'https://raw.githubusercontent.com/alexlee-dev/portfolio/master/public/favicon/favicon.ico'
   }
