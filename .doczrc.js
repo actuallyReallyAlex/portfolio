52eb8cd17c3b034e2bd95417284136141df255b8
Alex Lee <alex@alex-lee.site>
2018-09-28 21:26:12 -0700

    💥 Attempt to Build Docz and React at base '/docs/'

--- .doczrc.js
+++ .doczrc.js
@@ -1,5 +1,6 @@
 export default {
-  ordering: 'ascending'
+  ordering: 'ascending',
+  base: '/docs/'
   // htmlContext: {
   //   favicon: 'https://raw.githubusercontent.com/alexlee-dev/bookbag/master/src/assets/favicon/favicon.ico'
   // }
