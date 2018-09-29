b78003701cb023e8f305deadc0cc38adb0d97c2d
Alex Lee <alex@alex-lee.site>
2018-09-28 22:35:03 -0700

    🏗️ Attempt to Build Docs Again

---
+++ doczrc.js
@@ -0,0 +1,8 @@
+export default {
+  ordering: 'ascending',
+  dest: '/src/docs/',
+  base: '/docs/',
+  htmlContext: {
+    favicon: 'https://raw.githubusercontent.com/alexlee-dev/portfolio/master/public/favicon/favicon.ico'
+  }
+}
