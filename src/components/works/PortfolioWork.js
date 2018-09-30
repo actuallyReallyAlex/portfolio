01f49d4430b0d13767d17c4af8f389201ebc714c
Alex Lee <alex@alex-lee.site>
2018-09-30 11:28:55 -0700

    ✨ Create Works Components for Portfolio and PortfolioDocs

---
+++ src/components/works/PortfolioWork.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Tile, Card } from 'grommet'
+import PortfolioItemDescription from '../PortfolioItemDescription'
+
+const PortfolioWork = () => {
+  return (
+    <Tile>
+      <Card
+        colorIndex="accent-1"
+        thumbnail={
+          'https://res.cloudinary.com/alexlee-dev/image/upload/v1538331503/portfolio/portfolio.png'
+        }
+        heading="Portfolio"
+        label="Portfolio"
+        description={
+          <PortfolioItemDescription
+            languages={['React.js', 'JavaScript', 'Grommet']}
+            description="A portfolio build with React.js - an attempt to get away from repeated Bootstrap template portfolios. Also a chance to try out Grommet."
+          />
+        }
+      />
+    </Tile>
+  )
+}
+
+export default PortfolioWork
