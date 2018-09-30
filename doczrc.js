fdff6dcf4483cd55397cba76c2cd9846a0d3f756
Alex Lee <alex@alex-lee.site>
2018-09-30 08:41:31 -0700

    🎨 Add Grommet CSS to Documentation
    Helps with components rendering properly.

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,15 @@ export default {
   ordering: 'ascending',
   dest: '/src/docs/',
   htmlContext: {
-    favicon: 'https://raw.githubusercontent.com/alexlee-dev/portfolio/master/public/favicon/favicon.ico'
+    favicon:
+      'https://raw.githubusercontent.com/alexlee-dev/portfolio/master/public/favicon/favicon.ico',
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/grommet/1.11.0/grommet.min.css'
+        }
+      ]
+    }
   }
 }
