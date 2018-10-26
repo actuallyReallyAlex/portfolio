8c53505af086db6fb8885208396f247409f23aeb
Alex Lee <alex@alex-lee.site>
2018-10-26 11:37:34 -0700

    📝 Experience Matches Résumé

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,27 +12,19 @@ const Experience = () => {
       </Box>
       <Box direction="column" pad="large">
         <TimelineItem
-          title="HR Specialist"
-          startDate="June 2016"
+          title="Web Developer"
+          startDate="Summer 2016"
           present
-          icon="analytics"
+          icon="code"
           subtitle="Quantum Learning Network"
-          summary="Responsible for creating web tools to assist the SuperCamp Programs Department grow. Developed an overhaul of a staff website, and introduced new features to the public browsing the SuperCamp homepage. Designed and sent HTML newsletters to staff. Responsible for staffing the SuperCamp summer program - recruitment, interviewing, contracting over 100 new employees every year. Created numerous media campaigns as a key member of the SuperCamp Programs Department."
+          summary="Developed an integrated payroll information system for site administrators. Spearheaded a new staff branding by redesigning staff website for SuperCamp. Improved hiring efficiency by developing an employee contracting tool."
         />
         <TimelineItem
-          title="Administrative Assistant"
-          startDate="June 2014"
-          endDate="October 2015"
-          subtitle="Lincoln Property Management"
-          summary="Responsible for the recruitment and interviewing of staff. Coordinated meetings, prepared reports, and actively maintained quality assurance. Communicated with customers, employees, and the public to provide information and address concerns."
-        />
-        <TimelineItem
-          title="Lifeguard Instructor"
-          startDate="December 2014"
-          endDate="October 2015"
-          icon="aid"
-          subtitle="Lincoln Property Management"
-          summary="American Red Cross certified (Life Guard Instructor, Administering Emergency Oxygen, CPR, AED, First Aid, Lifeguarding, Blood-borne Pathogens, First Aid for Public Safety Personnel). Supervised the safety and rescue of all swimmers and water sports participants."
+          title="HR Specialist"
+          startDate="Spring 2016"
+          present
+          subtitle="Quantum Learning Network"
+          summary="Produced and designed monthly HTML newsletters. Shaped an improved interview process by interviewing over 100 employees yearly. Increased user reach by creating video and photo series for media campaigns."
         />
       </Box>
       <Box justify="center" textAlign="center">
@@ -52,7 +44,7 @@ const Experience = () => {
         <TimelineItem
           title="Associate's Degree"
           startDate=""
-          endDate="June 2017"
+          endDate="June 2016"
           icon="book"
           subtitle="MiraCosta College"
           summary="Biology Major with lab experience in Organic Chemistry, Molecular & Evolutionary Biology, and Foundational Physics."
