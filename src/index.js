15e78f956c1af5a1f32873003bb6b2a336db1835
Alex Lee <alex@alex-lee.site>
2019-04-14 20:29:26 -0700

    🔥 Remove React-GA

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom'
 import './customStyle.css'
 import Site from './Site'
 import registerServiceWorker from './registerServiceWorker'
-import ReactGA from 'react-ga'
 import * as Sentry from '@sentry/browser'
 import ErrorBoundary from './ErrorBoundary'
-ReactGA.initialize('UA-124507483-3')
-ReactGA.pageview('/home')
 
 Sentry.init({
   dsn: 'https://9bdeb775ed794184b80f143e62dae44d@sentry.io/1429953'
