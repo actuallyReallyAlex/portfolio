53da9c1ec0eaaa52e115c9bd8814c07f7ad33b0f
Alex Lee <alex@alex-lee.site>
2019-06-23 14:32:12 -0700

    ✏️ Change named export of component

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -3,13 +3,14 @@ import { Box, Heading } from 'grommet'
 import { social } from '../config.json'
 import SocialIcon from '../components/SocialIcon'
 
-const Section = () => {
+const Intro = () => {
   return (
     <Box
       align="center"
       background="url(
         'https://res.cloudinary.com/alexlee-dev/image/upload/c_scale,q_90,w_1152,f_auto/v1538630398/portfolio/portfolio_portland_dark_min.jpg'
       )"
+      fill="horizontal"
       height="400px"
       justify="center"
     >
@@ -39,4 +40,4 @@ const Section = () => {
   )
 }
 
-export default Section
+export default Intro
