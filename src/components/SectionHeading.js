88ee6c65b8fc44da6c5a8363801b68ecd37f556a
Alex Lee <alex@alex-lee.site>
2019-06-24 18:52:17 -0700

    ✨Create SectionHeading component

---
+++ src/components/SectionHeading.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box, Heading, Text } from 'grommet'
+
+const SectionHeading = ({ heading, subHeading }) => {
+  return (
+    <Box align="center" justify="center">
+      <Text className="uppercase">{heading}</Text>
+      <Heading level="2" margin={{ top: 'medium' }} size="large">
+        {subHeading}
+      </Heading>
+    </Box>
+  )
+}
+
+SectionHeading.propTypes = {
+  heading: PropTypes.string.isRequired,
+  subHeading: PropTypes.string.isRequired
+}
+
+export default SectionHeading
