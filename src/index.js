f7296a56e13482c4e350edad70e7fe565426ba1f
Alex Lee <alex@alex-lee.site>
2019-06-27 18:25:42 -0700

    📦 Add Sentry.io

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import * as Sentry from '@sentry/browser';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({dsn: "https://5a28f5da01f34b0283639b079218978e@sentry.io/1492278"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
