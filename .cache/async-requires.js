ed386f3233986e949d7b95afa9ab7378ecacd473
alexlee-dev <alex@alexlee.dev>
2019-10-09 10:15:33 -0700

    🚀 Gatsby Hello World

---
+++ .cache/async-requires.js
@@ -0,0 +1,8 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/alex/Repos/portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/alex/Repos/portfolio/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/alex/Repos/portfolio/.cache/dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/alex/Repos/portfolio/src/pages/index.js")))
+}
+
