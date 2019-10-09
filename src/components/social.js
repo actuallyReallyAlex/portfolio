45f1af9a3f8570fb8120ed793e992ce6cf0c16ab
alexlee-dev <alex@alexlee.dev>
2019-10-09 16:14:22 -0700

    ✏️ Social links work

--- src/components/social.js
+++ src/components/social.js
@@ -2,22 +2,47 @@ import React from "react";
 import { Box, Button, Icon } from "@material-ui/core";
 import socialStyles from "./social.module.scss";
 
+const links = {
+  mail: "mailto:alex@alexlee.dev",
+  github: "https://github.com/alexlee-dev/",
+  npm: "https://www.npmjs.com/~alexlee-dev",
+  twitter: "https://twitter.com/alexlee_dev",
+  linkedin: "https://www.linkedin.com/in/alexlee-dev"
+};
+
+const handleClick = linkType => window.open(links[linkType], "_blank");
+
 const Social = () => {
   return (
     <Box className={socialStyles.container}>
-      <Button className={socialStyles.socialButton}>
+      <Button
+        className={socialStyles.socialButton}
+        onClick={() => handleClick("mail")}
+      >
         <Icon className="far fa-envelope-open-text" />
       </Button>
-      <Button className={socialStyles.socialButton}>
+      <Button
+        className={socialStyles.socialButton}
+        onClick={() => handleClick("github")}
+      >
         <Icon className="fab fa-github" />
       </Button>
-      <Button className={socialStyles.socialButton}>
+      <Button
+        className={socialStyles.socialButton}
+        onClick={() => handleClick("npm")}
+      >
         <Icon className="fab fa-npm" />
       </Button>
-      <Button className={socialStyles.socialButton}>
+      <Button
+        className={socialStyles.socialButton}
+        onClick={() => handleClick("twitter")}
+      >
         <Icon className="fab fa-twitter" />
       </Button>
-      <Button className={socialStyles.socialButton}>
+      <Button
+        className={socialStyles.socialButton}
+        onClick={() => handleClick("linkedin")}
+      >
         <Icon className="fab fa-linkedin" />
       </Button>
     </Box>
