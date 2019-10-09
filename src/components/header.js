27a6aa05af65dfe71882548b8aa524b036f5326f
alexlee-dev <alex@alexlee.dev>
2019-10-09 10:47:40 -0700

    ✨ Basic Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
+import headerStyles from "./header.module.scss";
 
 const Header = () => {
-  return <header>Alex Lee</header>;
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography className={headerStyles.applicationTitle} variant="h6">
+          Alex Lee
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Blog</Button>
+        <Button color="inherit">About</Button>
+        <Button color="inherit">Résumé</Button>
+        <Button color="inherit">Contact</Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
