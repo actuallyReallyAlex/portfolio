5b4f47ed1029ba8a45fbd9e4278ae6f3c7ad25f2
alexlee-dev <alex@alexlee.dev>
2019-10-11 20:34:01 -0700

    Fix webp

--- public/component---src-pages-404-js-505af7ad2c33a3eb67b0.js
+++ public/component---src-pages-404-js-505af7ad2c33a3eb67b0.js
@@ -1,2 +1,66 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{293:function(e,t,a){"use strict";a.r(t);var n=a(0),i=a.n(n),r=a(288),o=a(344),s=a.n(o),c=a(312),l=a(310),m=a(313),h=a(60);t.default=function(){return i.a.createElement(r.a,{displayHeader:!0},i.a.createElement(c.a,{style:{marginTop:"100px",marginBottom:"100px",marginLeft:"30%",marginRight:"30%",display:"flex",flexDirection:"column",alignItems:"center"}},i.a.createElement(l.a,{style:{fontSize:"3rem"},variant:"h1"},"This Page is Buried in the Sand"),i.a.createElement("img",{alt:"Page not found",src:s.a,style:{maxHeight:"50vh"}}),i.a.createElement(l.a,{style:{marginBottom:"25px"}},"You have never seen an ostrich head. Whenever you’re around it seems ostriches are avoiding your gaze. You came on this trip specifically to see an ostrich head, but here is this ostrich right in front of you, head invisible. You may never see an ostrich head."),i.a.createElement(h.a,{style:{color:"inherit",textDecoration:"none"},to:"/"},i.a.createElement(m.a,{color:"primary",variant:"contained"},"Return Home"))))}},344:function(e,t,a){e.exports=a.p+"static/404-706017556960576aee293db2f4c2e041.png"}}]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [3],
+  {
+    293: function(e, t, a) {
+      "use strict";
+      a.r(t);
+      var n = a(0),
+        i = a.n(n),
+        r = a(288),
+        o = a(344),
+        s = a.n(o),
+        c = a(312),
+        l = a(310),
+        m = a(313),
+        h = a(60);
+      t.default = function() {
+        return i.a.createElement(
+          r.a,
+          { displayHeader: !0 },
+          i.a.createElement(
+            c.a,
+            {
+              style: {
+                marginTop: "100px",
+                marginBottom: "100px",
+                marginLeft: "30%",
+                marginRight: "30%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+              }
+            },
+            i.a.createElement(
+              l.a,
+              { style: { fontSize: "3rem" }, variant: "h1" },
+              "This Page is Buried in the Sand"
+            ),
+            i.a.createElement("img", {
+              alt: "Page not found",
+              src: s.a,
+              style: { maxHeight: "50vh" }
+            }),
+            i.a.createElement(
+              l.a,
+              { style: { marginBottom: "25px" } },
+              "You have never seen an ostrich head. Whenever you’re around it seems ostriches are avoiding your gaze. You came on this trip specifically to see an ostrich head, but here is this ostrich right in front of you, head invisible. You may never see an ostrich head."
+            ),
+            i.a.createElement(
+              h.a,
+              { style: { color: "inherit", textDecoration: "none" }, to: "/" },
+              i.a.createElement(
+                m.a,
+                { color: "primary", variant: "contained" },
+                "Return Home"
+              )
+            )
+          )
+        );
+      };
+    },
+    344: function(e, t, a) {
+      e.exports = a.p + "static/404-706017556960576aee293db2f4c2e041.png";
+    }
+  }
+]);
 //# sourceMappingURL=component---src-pages-404-js-505af7ad2c33a3eb67b0.js.map
--- public/component---src-templates-case-study-js-6aef3156f01f0de5f7b0.js
+++ public/component---src-templates-case-study-js-6aef3156f01f0de5f7b0.js
@@ -1,2 +1,94 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{291:function(e,t,a){"use strict";a.r(t),a.d(t,"query",(function(){return f}));a(75);var n=a(0),r=a.n(n),c=a(288),o=a(290),i=a(312),l=a(310),s=a(313),d=a(289),u=a(342),m=a.n(u),f="3520865454";t.default=function(e){var t={renderNode:{"embedded-asset-block":function(e){var t=e.data.target.fields.title,a=e.data.target.fields.file["en-US"],n=a.contentType,c=a.url;if(n.match(/(image)/))return r.a.createElement("img",{alt:t,src:c})}}},a=e.data.contentfulPortfolioWork,n=a.contentLink,u=a.description,f=a.screenshot,p=a.sourceLink,E=a.title;return r.a.createElement(c.a,{displayHeader:!0},r.a.createElement(i.a,{className:m.a.container},r.a.createElement(d.a,{title:E}),r.a.createElement(l.a,{variant:"h1"},E),r.a.createElement(i.a,{className:m.a.medSpace,fontStyle:"italic"},r.a.createElement(l.a,{variant:"subtitle1"},u)),r.a.createElement(i.a,{className:m.a.buttonContainer+" "+m.a.medSpace},n&&r.a.createElement(s.a,{color:"primary",onClick:function(){return window.open(n,"_blank")},variant:"contained"},"See it in action"),p&&r.a.createElement(s.a,{color:"secondary",onClick:function(){return window.open(p,"_blank")},variant:"contained"},"View source")),r.a.createElement("img",{alt:f.title,src:f.file.url}),r.a.createElement("hr",null),Object(o.documentToReactComponents)(e.data.contentfulPortfolioWork.caseStudy.json,t)))}}}]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [8],
+  {
+    291: function(e, t, a) {
+      "use strict";
+      a.r(t),
+        a.d(t, "query", function() {
+          return f;
+        });
+      a(75);
+      var n = a(0),
+        r = a.n(n),
+        c = a(288),
+        o = a(290),
+        i = a(312),
+        l = a(310),
+        s = a(313),
+        d = a(289),
+        u = a(342),
+        m = a.n(u),
+        f = "3520865454";
+      t.default = function(e) {
+        var t = {
+            renderNode: {
+              "embedded-asset-block": function(e) {
+                var t = e.data.target.fields.title,
+                  a = e.data.target.fields.file["en-US"],
+                  n = a.contentType,
+                  c = a.url;
+                if (n.match(/(image)/))
+                  return r.a.createElement("img", { alt: t, src: c });
+              }
+            }
+          },
+          a = e.data.contentfulPortfolioWork,
+          n = a.contentLink,
+          u = a.description,
+          f = a.screenshot,
+          p = a.sourceLink,
+          E = a.title;
+        return r.a.createElement(
+          c.a,
+          { displayHeader: !0 },
+          r.a.createElement(
+            i.a,
+            { className: m.a.container },
+            r.a.createElement(d.a, { title: E }),
+            r.a.createElement(l.a, { variant: "h1" }, E),
+            r.a.createElement(
+              i.a,
+              { className: m.a.medSpace, fontStyle: "italic" },
+              r.a.createElement(l.a, { variant: "subtitle1" }, u)
+            ),
+            r.a.createElement(
+              i.a,
+              { className: m.a.buttonContainer + " " + m.a.medSpace },
+              n &&
+                r.a.createElement(
+                  s.a,
+                  {
+                    color: "primary",
+                    onClick: function() {
+                      return window.open(n, "_blank");
+                    },
+                    variant: "contained"
+                  },
+                  "See it in action"
+                ),
+              p &&
+                r.a.createElement(
+                  s.a,
+                  {
+                    color: "secondary",
+                    onClick: function() {
+                      return window.open(p, "_blank");
+                    },
+                    variant: "contained"
+                  },
+                  "View source"
+                )
+            ),
+            r.a.createElement("img", { alt: f.title, src: f.file.url }),
+            r.a.createElement("hr", null),
+            Object(o.documentToReactComponents)(
+              e.data.contentfulPortfolioWork.caseStudy.json,
+              t
+            )
+          )
+        );
+      };
+    }
+  }
+]);
 //# sourceMappingURL=component---src-templates-case-study-js-6aef3156f01f0de5f7b0.js.map
