2e3b51799113d2b6ed0b5f976dbfd1238104d31e
alexlee-dev <alex@alexlee.dev>
2019-10-16 16:56:44 -0700

    Add forceFullSync option to gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,8 @@ const config = {
       resolve: "gatsby-source-contentful",
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        forceFullSync: true
       }
     }
   ]
