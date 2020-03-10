9b9049f7ac9752be6f5871c50192392c661eea9b
alexlee-dev <alex@alexlee.dev>
2020-03-10 11:55:13 -0700

    Static Asset query change

--- src/sections/about.js
+++ src/sections/about.js
@@ -19,14 +19,17 @@ const AboutSection = () => {
           }
         }
       }
-      contentfulAsset(title: { eq: "Resume" }) {
-        title
-        file {
-          url
+      contentfulStaticAsset(contentful_id: { eq: "2se5vxRWlbFKVwdeOlyuOm" }) {
+        content {
+          file {
+            url
+          }
         }
       }
     }
   `);
+
+  console.log(data);
 
   return (
     <Box
@@ -43,7 +46,7 @@ const AboutSection = () => {
       <Box className={aboutStyles.buttonContainer}>
         <Button
           className={aboutStyles.resume}
-          href={data.contentfulAsset.file.url}
+          href={data.contentfulStaticAsset.content.file.url}
           target="_blank"
           variant="contained"
         >
