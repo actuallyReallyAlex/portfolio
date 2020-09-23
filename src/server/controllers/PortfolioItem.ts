8acaaed01ed1365c1dc795f679c0960c2c376d6c
alexlee-dev <alex@alexlee.dev>
2020-09-23 08:27:19 -0700

    📃 Display List of Real Portfolio Items #353 - Fetch real items

--- src/server/controllers/PortfolioItem.ts
+++ src/server/controllers/PortfolioItem.ts
@@ -1,8 +1,5 @@
 import express, { Request, Response, Router } from "express";
-import path from "path";
 import PortfolioItemModel from "../models/PortfolioItem";
-
-import { PortfolioItem } from "../types";
 
 class PortfolioItemController {
   public router: Router = express.Router();
@@ -16,25 +13,9 @@ class PortfolioItemController {
       "/portfolioItems",
       async (req: Request, res: Response): Promise<Response<any>> => {
         try {
-          const mockPortfolioItems: PortfolioItem[] = [
-            {
-              content: "This project is cool.",
-              tagline: "This is a super cool project.",
-              title: "The Cool Project",
-            },
-            {
-              content: "This project is lame.",
-              tagline: "This is a super lame project.",
-              title: "The Lame Project",
-            },
-            {
-              content: "This project is blue.",
-              tagline: "This is a super blue project.",
-              title: "The Blue Project",
-            },
-          ];
+          const portfolioItemDocuments = await PortfolioItemModel.find({});
 
-          return res.send(mockPortfolioItems);
+          return res.send(portfolioItemDocuments);
         } catch (error) {
           console.error(error);
           return res.status(500).send({ error });
