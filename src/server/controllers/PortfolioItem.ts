32c0ebe4ce0b090670e75e332c8c6f169fa0fe58
alexlee-dev <alex@alexlee.dev>
2020-09-22 21:57:14 -0700

    🗒️ Display List of Mock Portfolio Items #352 - Create mock data

--- src/server/controllers/PortfolioItem.ts
+++ src/server/controllers/PortfolioItem.ts
@@ -15,7 +15,25 @@ class PortfolioItemController {
       "/portfolioItems",
       async (req: Request, res: Response): Promise<Response<any>> => {
         try {
-          return res.send("Hello, from Server! :)");
+          const mockPortfolioItems: PortfolioItem[] = [
+            {
+              content: "This project is cool.",
+              tagline: "This is a super cool project.",
+              title: "The Cool Project",
+            },
+            {
+              content: "This project is lame.",
+              tagline: "This is a super lame project.",
+              title: "The Lame Project",
+            },
+            {
+              content: "This project is blue.",
+              tagline: "This is a super blue project.",
+              title: "The Blue Project",
+            },
+          ];
+
+          return res.send(mockPortfolioItems);
         } catch (error) {
           console.error(error);
           return res.status(500).send({ error });
