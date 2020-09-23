775db1c69d97702c2c161728107e200c94336558
alexlee-dev <alex@alexlee.dev>
2020-09-22 20:53:32 -0700

    👋 Hello World #349 - Add functionality

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const config = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  // optimization: {
+  //   moduleIds: "hashed",
+  //   runtimeChunk: "single",
+  //   splitChunks: {
+  //     chunks: "all",
+  //     cacheGroups: {
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name: "vendors",
+  //         chunks: "all",
+  //       },
+  //     },
+  //   },
+  // },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    // publicPath: "dist/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.html"),
+    }),
+  ],
+  // resolve: {
+  //   extensions: [".tsx"],
+  // },
+};
+
+module.exports = config;
