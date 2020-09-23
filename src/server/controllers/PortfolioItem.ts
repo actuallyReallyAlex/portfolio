6df89696956398fe341155f62584767bf0bc4a44
alexlee-dev <alex@alexlee.dev>
2020-09-22 21:54:55 -0700

    🗒️ Display List of Mock Portfolio Items #352 - Create endpoint

--- src/server/controllers/PortfolioItem.ts
+++ src/server/controllers/PortfolioItem.ts
@@ -10,7 +10,19 @@ class PortfolioItemController {
     this.initializeRoutes();
   }
 
-  public initializeRoutes(): void {}
+  public initializeRoutes(): void {
+    this.router.get(
+      "/portfolioItems",
+      async (req: Request, res: Response): Promise<Response<any>> => {
+        try {
+          return res.send("Hello, from Server! :)");
+        } catch (error) {
+          console.error(error);
+          return res.status(500).send({ error });
+        }
+      }
+    );
+  }
 }
 
 export default PortfolioItemController;
