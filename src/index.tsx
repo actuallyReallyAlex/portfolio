81fcd057b0f524c2565a49f95da3e0544d593133
Alex Lee <alex@alexlee.dev>
2022-03-13 20:11:42 -0700

    ✨ feature: only FE Hello World

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <span>Hello, World</span>;
+};
+
+const container = document.getElementById("root");
+
+ReactDOM.render(<App />, container);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
