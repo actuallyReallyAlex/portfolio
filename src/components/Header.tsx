e08a40d17e9f43380bd769820361cf90f93b2ee4
Alex Lee <alex@alexlee.dev>
2022-03-29 08:13:38 -0700

    ✨ feature: add smooth scroll link clicking from Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 
+// TODO - More readable once user scrolls past top of page (?)
 export const Header = () => {
   return (
     <header>
       <div>
         <main>
-          <a href="#">HOME</a>
+          <button
+            id="home-button"
+            onClick={() => scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+          >
+            HOME
+          </button>
         </main>
         <nav>
           <ul>
-            {/* <li className="nav-item">Work</li> */}
+            <li className="nav-item">
+              <button
+                onClick={() =>
+                  document
+                    .getElementById("projects")
+                    .scrollIntoView({ block: "start", behavior: "smooth" })
+                }
+              >
+                WORK
+              </button>
+            </li>
             <li className="nav-item">
               <a href="mailto:actuallyreallyalex@gmail.com">CONTACT</a>
             </li>
